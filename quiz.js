2f0a3b13e807490069616eec526d9b83a9b6acdb
Michael Mead <michaelmead007@icloud.com>
2016-04-18 13:37:06 -0500

    complete

--- quiz.js
+++ quiz.js
@@ -2,16 +2,24 @@ var tree = {
 	height: "",
 	character: ""
 };
+var row = "";
+var space = " ";
 
 function buildTree (tree) {
 	tree.height = document.getElementById("height").value;
 	// console.log("height innerHTML:", document.getElementById("height").value);
 	tree.character = document.getElementById("character").value;
-	for (var i = 0; i < tree.height ; i++) {
-
-		for (var n = 0; n < tree.character; n++) {
-			
+	for (var i = 1; i <= tree.height ; i+= 1) {
+		// console.log("i:", i);
+			row += tree.character; 
+			space = " ";
+			if (i>1) {
+				row += tree.character;
+			};
+		for (var n = tree.height-i; n >= 1; n--) {
+			space += " "; 
 		};
+		console.log("row:", space + row);
 	};
 	console.log("tree height:", tree.height);
 	console.log("tree char.", tree.character);
